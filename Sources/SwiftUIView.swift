8dd5bee9baf97227124692e1cfbc3ec1f1f206db
tarmi-81 <jozef.gmuca@gmail.com>
2022-12-07 21:24:35 +0100

    update

--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public struct SwiftUIView: View {
-   
+    public init() {}
     public var body: some View {
         Text("This is a SwiftUIView from package ")
     }
